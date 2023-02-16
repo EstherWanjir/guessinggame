70f230610fe65866633262166d1a5b4152c041d7
Estherwanjir <estherwachira1425@gmail.com>
2023-02-16 10:19:34 +0300

    added hello message

--- game.py
+++ game.py
@@ -2,14 +2,40 @@
 from random import randint
 #get user input
 user=input("Enter username:")
-print("Hello there " + user + "!")
+print("Hello there, " + user + "!")
 
 #generate  a random number
 number=randint(15,30)
 
 counter=0
 while counter <5:
-    userno=eval(input("Enter your number:"))
+    userno=eval(input("Enter a random  number:"))
+    counter += 1 #same as counter=counter+1
+
+    if userno < number :
+        print("Sorry, your guess is too low.")
+
+    elif userno  > number :
+        print("Sorry , your guess is too high.")
+    elif userno  == number:
+        break
+     
+             
+
+print("Game over.")
+
+
+if userno==number:
+    print("Congrats! You won!")
+else:
+    print("Sorry, The correct number was " ) 
+    print(number)   
+
+
+
+
+
+
 
 
 
